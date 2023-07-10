a05be336244590cf927ec43fb69352fd0e7f0b3c
Raigo-Robin Anni <92438586+Raigo-Robin@users.noreply.github.com>
2023-07-11 01:04:11 +0300

    Update sentry.js

---
+++ sentry/sentry.js
@@ -0,0 +1,22 @@
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: "https://255cf149c69d405da08760c17416a5e6@o4505507285237760.ingest.sentry.io/4505507292315648",
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    // Intentional error to test Sentry
+    throw new Error("This is a test error");
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
