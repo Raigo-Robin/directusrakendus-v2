5c6c2f743d419db3909b214198b751b1fcf8bf20
Raigo-Robin Anni <92438586+Raigo-Robin@users.noreply.github.com>
2023-07-11 01:07:51 +0300

    Update sentry.js

--- sentry/sentry.js
+++ sentry/sentry.js
@@ -1,7 +1,7 @@
 const Sentry = require("@sentry/node");
 
 Sentry.init({
-  dsn: "YOUR_SENTRY_DSN",
+  dsn: "https://255cf149c69d405da08760c17416a5e6@o4505507285237760.ingest.sentry.io/4505507292315648",
   tracesSampleRate: 1.0,
 });
 
