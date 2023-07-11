886b1cb4b15006a3a0dc5cca30cc5d1b6860ecef
Raigo-Robin Anni <92438586+Raigo-Robin@users.noreply.github.com>
2023-07-11 08:43:43 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: "https://255cf149c69d405da08760c17416a5e6@o4505507285237760.ingest.sentry.io/4505507292315648",
+  tracesSampleRate: 1.0,
+});
+
+// Your existing Directus container code here
+// ...
