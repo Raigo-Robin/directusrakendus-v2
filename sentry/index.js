46757a661844fd752162fd9247c4d82fa7fcf624
Raigo-Robin Anni <92438586+Raigo-Robin@users.noreply.github.com>
2023-07-11 08:15:14 +0300

    Update index.js

--- sentry/index.js
+++ sentry/index.js
@@ -5,18 +5,5 @@ Sentry.init({
   tracesSampleRate: 1.0,
 });
 
-const transaction = Sentry.startTransaction({
-  op: "test",
-  name: "My First Test Transaction",
-});
-
-setTimeout(() => {
-  try {
-    // Intentional error to test Sentry
-    throw new Error("This is a test error");
-  } catch (e) {
-    Sentry.captureException(e);
-  } finally {
-    transaction.finish();
-  }
-}, 99);
+// Your remaining application code goes here
+// ...
