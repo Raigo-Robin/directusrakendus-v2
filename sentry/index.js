0fdfb62ddca638e796673d8f9d2e2a35dd935af5
Raigo-Robin Anni <92438586+Raigo-Robin@users.noreply.github.com>
2023-07-13 23:03:27 +0300

    Update index.js

--- sentry/index.js
+++ sentry/index.js
@@ -1,6 +1,31 @@
+yarn add @sentry/node
+
+npm install --save @sentry/node
+
 const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
 
 Sentry.init({
   dsn: "https://255cf149c69d405da08760c17416a5e6@o4505507285237760.ingest.sentry.io/4505507292315648",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
 });
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
