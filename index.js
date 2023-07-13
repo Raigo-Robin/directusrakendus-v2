f045dcb6c8a7b55b6001b579522bbcefb47e66ec
Raigo-Robin Anni <92438586+Raigo-Robin@users.noreply.github.com>
2023-07-13 23:19:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ Sentry.init({
   tracesSampleRate: 1.0,
 });
 
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
