36835af2d6fabeda272da524a066740cb7e67d17
Raigo-Robin Anni <92438586+Raigo-Robin@users.noreply.github.com>
2023-07-14 12:01:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
+// Environment
+require('dotenv').config();
+
 // Sentry
-const Sentry = require('@sentry/node'); 
+const Sentry = require('@sentry/node');
 Sentry.init({
-  dsn: process.env.SENTRY_DSN
+  dsn: process.env.SENTRY_DSN  
 });
 
 // MySQL
-const mysql = require('mysql2/promise');
-const db = await mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  ssl: {
-    rejectUnauthorized: true 
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: true
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('MySQL connection error: ' + err.stack);
+    return;
   }
+  
+  console.log('Connected to MySQL!');
+  
+  connection.query('SELECT 1', (err, results) => {
+    if (err) throw err;
+    console.log('Query test: ' + results); 
+  });
 });
 
-// Test MySQL
-await db.query('SELECT 1');
+// Directus
+const directus = require('@directus/sdk');
+directus.createApp({
+  database: {
+    client: 'mysql',
+    connection: connection
+  }
+})
 
-// Pass MySQL connection to Directus
-// ...
+// Express
+const app = require('express')();
 
-// Start Directus
-directusStart();
-
-// Express app
-const app = require('express')(); 
-
-// Routes...
-
-// Error test route
 app.get('/error', () => {
-  throw new Error('Test error');
+  throw new Error('Test error'); 
 });
 
-// Error handler
 app.use(Sentry.Handlers.errorHandler());
 
-// Start server
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`) 
+  console.log(`App listening on port ${port}`);
 });
