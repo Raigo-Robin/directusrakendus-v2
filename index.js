c74cf270c09841cb14fcbd4fc7e2fc9c1d44a6bc
Raigo-Robin Anni <92438586+Raigo-Robin@users.noreply.github.com>
2023-07-14 11:37:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
-// Sentry 
-const Sentry = require('@sentry/node');
+// Sentry
+const Sentry = require('@sentry/node'); 
 Sentry.init({
   dsn: process.env.SENTRY_DSN
 });
 
 // MySQL
 const mysql = require('mysql2/promise');
-
-// Create MySQL connection
-const db = await mysql.createConnection({ 
+const db = await mysql.createConnection({
   host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER, 
+  user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: true 
   }
 });
 
-// Test MySQL connection 
+// Test MySQL
 await db.query('SELECT 1');
 
-// Directus
-require('./directus')(); 
+// Pass MySQL connection to Directus
+// ...
+
+// Start Directus
+directusStart();
 
 // Express app
-const app = require('express')();
+const app = require('express')(); 
 
-// Routes... 
+// Routes...
 
-// Sentry error handling middleware
+// Error test route
+app.get('/error', () => {
+  throw new Error('Test error');
+});
+
+// Error handler
 app.use(Sentry.Handlers.errorHandler());
 
 // Start server
-const port = 3000;
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`Server started on port ${port}`) 
 });
